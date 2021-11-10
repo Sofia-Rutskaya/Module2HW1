1eed13056d7d482cdc77f8dbd89d320f1cb55dbe
Sofia_Rutska <sofiia.rutska@nure.ua>
2021-11-10 09:09:53 +0200

    Logger is done

---
+++ StyleCop-master/StyleCop/StyleCop/Logger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace StyleCop
+{
+    public class Logger
+    {
+        private static Logger instance;
+        private DateTime _date1;
+        private StringBuilder _information;
+
+        private Logger()
+        {
+            _information = new StringBuilder("\n");
+        }
+
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+
+            return instance;
+        }
+
+        public void Print(TypeLog lof, string message)
+        {
+            _date1 = DateTime.Now;
+            Console.WriteLine($"{_date1}: {lof}: {message}");
+            SaveInfo(_date1, lof, message);
+        }
+
+        public void SaveInfo(DateTime date, TypeLog lof, string message)
+        {
+           _information.Append($"{_date1}: {lof}: {message}\n");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine(_information);
+        }
+    }
+}
--- StyleCop-master/StyleCop/StyleCop/Program.cs
+++ StyleCop-master/StyleCop/StyleCop/Program.cs
@@ -6,6 +6,15 @@ namespace StyleCop
     {
         public static void Main(string[] args)
         {
+            Logger log = Logger.GetInstance();
+            log.Print(TypeLog.Warning, "Something happen");
+            log.Print(TypeLog.Info, "Something happened!!!");
+            log.Print(TypeLog.Error, "Sg happen");
+            log.Print(TypeLog.Warning, "Something happen");
+
+            log.GetInfo();
+
+            // Logger.Print();
         }
     }
 }
